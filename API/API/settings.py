9ed2faaa517d353ddddf5aa3e415bd7857747c65
Victor Sagawa <sagawahx3@hotmail.com>
2023-04-23 18:21:18 -0300

    settings update

--- API/API/settings.py
+++ API/API/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-518-4&30kc0i)w8yrms^c!j9+4qepuw08h#a78szx*sy8d(!8f'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
